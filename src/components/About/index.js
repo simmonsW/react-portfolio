2d3fbd44fcd4aaed2d8ba116896f90ff9c237995
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-15 01:34:12 -0700

    added initial About me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function About() {
   return(
-    <h1>This is the about page!</h1>
+    <section className="about-me">
+      <h1 id="about">
+        About Me
+      </h1>
+
+      <div className="about-content">
+        <img src="https://via.placeholder.com/300.png/?text=Dis+is+me" />
+        <p className="about-description">
+          Welcome! My name is Wyatt Simmons and thanks for checking out my website! I
+          am a soon to be graduate from the UCLA coding Bootcamp. I enjoy working with 
+          backend tools, as well as JavaScript. I enjoy working through problems to 
+          find solutions. Check out the Portfolio tab to see some of my latest work.
+        </p>
+      </div>
+    </section>
   )
 };
 
