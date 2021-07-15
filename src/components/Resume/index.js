a387464becf8b0e60487654e30aeebf2a401e2f9
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-15 12:11:56 -0700

    initial resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 function Resume() {
+
+  const proficiencyArr = [
+    'Javascript',
+    'CSS',
+    'HTML',
+    'Node.js',
+    'Express.js',
+    'React',
+    'MongoDB',
+    'MySQL',
+    'Sequelize',
+    'GraphQL',
+  ];
+
   return(
-    <h1>This is the Resume page!</h1>
+    <section className="resume-page">
+      <div id="resume-title">
+        <h1>
+          Resume
+        </h1>
+      </div>
+      <div className="prof-list-container">
+        <h2 className="prof-list-title">
+          Proficiencies
+        </h2>
+        <ul className="prof-list">
+          {proficiencyArr.map(prof => (
+            <li key={prof.index} className="prof">
+              {prof}
+            </li>
+          ))}
+        </ul>
+        <a href="https://www.google.com">
+          <h3>
+            Download Resume
+          </h3>
+        </a>
+      </div>
+    </section>
   )
 };
 
