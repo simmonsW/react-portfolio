c942d23b789708c2f76d7c7813d36c27ade16608
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-26 00:27:03 -0700

    added ChelcieFans and employee tracker, renamed project-images, conditional launch/youtube icon

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,51 +1,81 @@
 import React, { useState } from 'react';
 import { GrLaunch } from 'react-icons/gr';
-import { RiGitRepositoryLine } from 'react-icons/ri';
+import { RiGitRepositoryLine, RiYoutubeLine } from 'react-icons/ri';
 
 const Project = ({ name }) => {
   const [projects] = useState([
     {
       id: 1,
+      projectName: 'ChelcieFans',
+      projDesc: "A place for Chelcie's fans to discuss all things Chelcie",
+      deployedProj: 'https://secret-hollows-41476.herokuapp.com/',
+      githubRepo: 'https://github.com/simmonsW/chelcie-fans',
+      imageName: 'chelcie-fans',
+      demoVid: false
+    },
+    {
+      id: 2,
       projectName: 'Pawpals',
       projDesc: "A social platform to find furry friends for your pets!",
       deployedProj: 'https://noemiegrau.github.io/pawpals/',
       githubRepo: 'https://github.com/Noemiegrau/pawpals',
-    },
-    {
-      id: 2,
-      projectName: 'Rate My Ride',
-      projDesc: "A social platform for users to show off their car and check out other user's cars",
-      deployedProj: 'https://rate-my-ride.herokuapp.com/',
-      githubRepo: 'https://github.com/albertjly/rate-my-ride'
+      imageName: 'pawpals',
+      demoVid: false
     },
     {
       id: 3,
-      projectName: 'Weather Dashboard',
-      projDesc: 'A weather app where users can search for weather based on a city, and searched cities are saved to search again.',
-      deployedProj: 'https://simmonsw.github.io/weather-dashboard/',
-      githubRepo: 'https://github.com/simmonsW/weather-dashboard'
+      projectName: 'Rate My Ride',
+      projDesc: "A social platform for users to show off their car and check out other user's cars",
+      deployedProj: 'https://rate-my-ride.herokuapp.com/',
+      githubRepo: 'https://github.com/albertjly/rate-my-ride',
+      imageName: 'rate-my-ride',
+      demoVid: false
     },
     {
       id: 4,
-      projectName: 'Work Day Scheduler',
-      projDesc: 'A schedule app for users to plan their work day',
-      deployedProj: 'https://simmonsw.github.io/work-day-scheduler/',
-      githubRepo: 'https://github.com/simmonsW/work-day-scheduler'
+      projectName: 'Employee Tracker',
+      projDesc: 'A CMS, or content management system, for tracking employees, departments, roles, etc.',
+      deployedProj: 'https://www.youtube.com/watch?v=jodcGBJgRyQ',
+      githubRepo: 'https://github.com/simmonsW/employee-tracker',
+      imageName: 'employee-tracker',
+      demoVid: true
     },
     {
       id: 5,
-      projectName: 'Code Quiz',
-      projDesc: 'A simple coding quiz to test users on some front-end knowledge',
-      deployedProj: 'https://simmonsw.github.io/reimagined-disco/',
-      githubRepo: 'https://github.com/simmonsW/reimagined-disco'
+      projectName: 'Weather Dashboard',
+      projDesc: 'A weather app where users can search for weather based on a city, and searched cities are saved to search again.',
+      deployedProj: 'https://simmonsw.github.io/weather-dashboard/',
+      githubRepo: 'https://github.com/simmonsW/weather-dashboard',
+      imageName: 'weather-dashboard',
+      demoVid: false
     },
     {
       id: 6,
-      projectName: 'Password Generator',
-      projDesc: 'A simple password generator',
-      deployedProj: 'https://simmonsw.github.io/upgraded-fiesta/',
-      githubRepo: 'https://github.com/simmonsW/upgraded-fiesta'
-    }
+      projectName: 'Work Day Scheduler',
+      projDesc: 'A schedule app for users to plan their work day',
+      deployedProj: 'https://simmonsw.github.io/work-day-scheduler/',
+      githubRepo: 'https://github.com/simmonsW/work-day-scheduler',
+      imageName: 'work-day-scheduler',
+      demoVid: false
+    },
+    // {
+    //   id: 7,
+    //   projectName: 'Code Quiz',
+    //   projDesc: 'A simple coding quiz to test users on some front-end knowledge',
+    //   deployedProj: 'https://simmonsw.github.io/reimagined-disco/',
+    //   githubRepo: 'https://github.com/simmonsW/reimagined-disco',
+    //   imageName: 'code-quiz',
+    //   demoVid: false
+    // },
+    // {
+    //   id: 7,
+    //   projectName: 'Password Generator',
+    //   projDesc: 'A simple password generator',
+    //   deployedProj: 'https://simmonsw.github.io/upgraded-fiesta/',
+    //   githubRepo: 'https://github.com/simmonsW/upgraded-fiesta',
+    //   imageName: 'password-generator',
+    //   demoVid: false
+    // }
   ]);
 
   const currentProjects = projects.filter((project) => project.name === name);
@@ -57,11 +87,13 @@ const Project = ({ name }) => {
         projectName,
         projDesc,
         deployedProj,
-        githubRepo
+        githubRepo,
+        imageName,
+        demoVid
       }) => (
         <div key={id} className="project">
           <img
-            src={require(`../../assets/project-images/${id}.png`).default}
+            src={require(`../../assets/project-images/${imageName}.png`).default}
             alt={projectName}
             className="project-img"
           />
@@ -80,13 +112,26 @@ const Project = ({ name }) => {
 
           <div className="project-links">
             <div>
-              <a href={deployedProj}>
-                <GrLaunch />
+              <a
+                target="_blank"
+                rel="noopener noreferrer" 
+                href={deployedProj}
+              >
+                {demoVid === false && (
+                  <GrLaunch />
+                )}
+                {demoVid === true && (
+                  <RiYoutubeLine className="youtube-link"/>
+                )}
               </a>
             </div>
 
             <div>
-              <a href={githubRepo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"  
+                href={githubRepo}
+              >
                 <RiGitRepositoryLine />
               </a>
             </div>
