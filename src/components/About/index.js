a85f72c1f6261935ecab40c51faad08e69720c67
Wyatt Simmons <simmonsw182@gmail.com>
2022-01-27 15:16:21 -0800

    updated About Me description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,9 +14,9 @@ function About() {
         />
         <p className="about-description">
           Welcome! My name is Wyatt Simmons and thanks for checking out my website! I
-          am a recent graduate from the UCLA coding Bootcamp. I enjoy working with 
-          backend tools, and especially with JavaScript. I enjoy working through problems to 
-          find solutions. Check out the Portfolio tab to see some of my latest work.
+          am a recent graduate from the UCLA coding Bootcamp. I enjoy working with front-end 
+          tools, especially JavaScript, as well as the back-end. I enjoy working through problems to 
+          find effective solutions. Check out the Portfolio tab to see some of my latest work.
         </p>
       </div>
     </section>
